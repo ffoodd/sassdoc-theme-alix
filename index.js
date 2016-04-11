8dfc9509f70d9965f72ff6dda4bcb9c9523d389c
ffoodd <gael.poupard@gmail.com>
2016-04-11 11:14:03 +0200

    Still template not found error

--- index.js
+++ index.js
@@ -1,146 +1,54 @@
-var path = require('path');
+var def = require('./default-configuration.json');
 
-var nunjucks = require('nunjucks');
-nunjucks.configure('views');
+var path          = require('path');
+var nunjucks      = require('nunjucks');
+var fsp           = require('fs-promise');
+var sassdocExtras = require('sassdoc-extras');
 
-/**
- * Themeleon template helper, using consolidate.js module.
- *
- * See <https://github.com/themeleon/themeleon>.
- * See <https://github.com/tj/consolidate.js>.
- */
-var themeleon = require('themeleon')();
-themeleon.use('consolidate');
+nunjucks = nunjucks.configure('views');
 
-/**
- * Utility function we will use to merge a default configuration
- * with the user object.
- */
-var extend = require('extend');
+/* Le rendu par Nunjucks */
+var renderFile = function renderFile(dest, ctx) {
+  return new Promise(function (resolve, reject) {
+    nunjucks.render(dest, ctx, function (err, res) {
+      if (err) reject(err);
+      else resolve(res);
+    });
+  });
+};
 
-/**
- * SassDoc extras (providing Markdown and other filters, and different way to
- * index SassDoc data).
- *
- * See <https://github.com/SassDoc/sassdoc-extras>.
- */
-var extras = require('sassdoc-extras');
+/* Les champs de base */
+var applyDefaults = function applyDefaults(ctx) {
+  return Object.assign({}, def, ctx, {
+    // Apply default values for groups and display.
+    groups: Object.assign(def.groups, ctx.groups),
+    display: Object.assign(def.display, ctx.display)
+  });
+};
 
-/**
- * The theme function. You can directly export it like this:
- *
- *     module.exports = themeleon(__dirname, function (t) {});
- *
- * ... but here we want more control on the template variables, so there
- * is a little bit of preprocessing below.
- *
- * The theme function describes the steps to render the theme.
- */
-var theme = themeleon(__dirname, function (t) {
-  t.copy('assets');
-  t.nunjucks(path.resolve(__dirname, 'views/index.html'));
-});
 
-/**
- * Actual theme function. It takes the destination directory `dest`
- * (that will be handled by Themeleon), and the context variables `ctx`.
- *
- * Here, we will modify the context to have a `view` key defaulting to
- * a literal object, but that can be overriden by the user's
- * configuration.
- */
 module.exports = function (dest, ctx) {
-  var def = {
-    display: {
-      annotations: {
-        function: ["description", "parameter", "return", "example", "throw", "require", "usedby", "since", "see", "todo", "link", "author"],
-        mixin: ["description", "parameter", "content", "output", "example", "throw", "require", "usedby", "since", "see", "todo", "link", "author"],
-        placeholder: ["description", "example", "throw", "require", "usedby", "since", "see", "todo", "link", "author"],
-        variable: ["description", "type", "property", "require", "example", "usedby", "since", "see", "todo", "link", "author"]
-      },
-      access: ['public', 'private'],
-      alias: false,
-      watermark: true,
-    },
-    groups: {
-      'undefined': 'General',
-    },
-    shortcutIcon: 'http://ffoodd.github.io/a11y.css/static/favicon.ico',
-  };
+  var ctx = applyDefaults(ctx);
+  sassdocExtras(
+    ctx,
+    'description',
+    'markdown',
+    'display',
+    'groupName',
+    'shortcutIcon',
+    'sort',
+    'resolveVariables'
+  );
+  ctx.data.byGroupAndType = sassdocExtras.byGroupAndType(ctx.data);
 
-  // Apply default values for groups and display.
-  ctx.groups = extend(def.groups, ctx.groups);
-  ctx.display = extend(def.display, ctx.display);
+  var initialAssetPath = path.resolve(__dirname, '../assets');
+  var finalAssetPath   = path.resolve(dest, 'assets');
+  var destination      = path.resolve(dest, 'index.html');
 
-  // Extend top-level context keys.
-  ctx = extend({}, def, ctx);
-
-  /**
-   * Parse text data (like descriptions) as Markdown, and put the
-   * rendered HTML in `html*` variables.
-   *
-   * For example, `ctx.package.description` will be parsed as Markdown
-   * in `ctx.package.htmlDescription`.
-   *
-   * See <http://sassdoc.com/extra-tools/#markdown>.
-   */
-  extras.markdown(ctx);
-
-  /**
-   * Add a `display` property for each data item regarding of display
-   * configuration (hide private items and aliases for example).
-   *
-   * You'll need to add default values in your `.sassdocrc` before
-   * using this filter:
-   *
-   *     {
-   *       "display": {
-   *         "access": ["public", "private"],
-   *         "alias": false
-   *       }
-   *     }
-   *
-   * See <http://sassdoc.com/extra-tools/#display-toggle>.
-   */
-  extras.display(ctx);
-
-  /**
-   * Allow the user to give a name to the documentation groups.
-   *
-   * We can then have `@group slug` in the docblock, and map `slug`
-   * to `Some title string` in the theme configuration.
-   *
-   * **Note:** all items without a group are in the `undefined` group.
-   *
-   * See <http://sassdoc.com/extra-tools/#groups-aliases>.
-   */
-  extras.groupName(ctx);
-
-  /**
-   * Use SassDoc indexer to index the data by group and type, so we
-   * have the following structure:
-   *
-   *     {
-   *       "group-slug": {
-   *         "function": [...],
-   *         "mixin": [...],
-   *         "variable": [...]
-   *       },
-   *       "another-group": {
-   *         "function": [...],
-   *         "mixin": [...],
-   *         "variable": [...]
-   *       }
-   *     }
-   *
-   * You can then use `data.byGroupAndType` instead of `data` in your
-   * templates to manipulate the indexed object.
-   */
-  ctx.data.byGroupAndType = extras.byGroupAndType(ctx.data);
-
-  /**
-   * Now we have prepared the data, we can proxy to the Themeleon
-   * generated theme function.
-   */
-  return theme.apply(this, arguments);
+  return Promise.all([
+    fsp.copy(initialAssetPath, finalAssetPath),
+    renderFile('index.html', ctx).then(function (html) {
+      return fsp.writeFile(destination, html);
+    })
+  ]);
 };
